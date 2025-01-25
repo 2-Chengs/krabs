971ec58f81b652d0182b3e332fd8e4deca74b61f
Ivan Cheng <ivan.cheng011099@gmail.com>
2025-01-24 20:58:19 -0800

    fixed build bug on this file

--- client/components/particle-background.tsx
+++ client/components/particle-background.tsx
@@ -1,6 +1,42 @@
 "use client"
 
-import { useEffect, useRef } from "react"
+import React, { useEffect, useRef } from "react"
+
+class Particle {
+  x: number
+  y: number
+  size: number
+  speedX: number
+  speedY: number
+  color: string
+
+  constructor(canvas: HTMLCanvasElement) {
+    this.x = Math.random() * canvas.width
+    this.y = Math.random() * canvas.height
+    this.size = Math.random() * 3 + 1
+    this.speedX = Math.random() * 3 - 1.5
+    this.speedY = Math.random() * 3 - 1.5
+    this.color = `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 0.7)`
+  }
+
+  update(canvas: HTMLCanvasElement) {
+    this.x += this.speedX
+    this.y += this.speedY
+
+    if (this.x > canvas.width) this.x = 0
+    else if (this.x < 0) this.x = canvas.width
+
+    if (this.y > canvas.height) this.y = 0
+    else if (this.y < 0) this.y = canvas.height
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.fillStyle = this.color
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
+    ctx.fill()
+  }
+}
 
 export function ParticleBackground() {
   const canvasRef = useRef<HTMLCanvasElement>(null)
@@ -16,72 +52,36 @@ export function ParticleBackground() {
     canvas.height = window.innerHeight
 
     const particles: Particle[] = []
-    const particleCount = 50
-
-    class Particle {
-      x: number
-      y: number
-      size: number
-      speedX: number
-      speedY: number
-      color: string
-
-      constructor() {
-        this.x = Math.random() * canvas.width
-        this.y = Math.random() * canvas.height
-        this.size = Math.random() * 3 + 1
-        this.speedX = Math.random() * 3 - 1.5
-        this.speedY = Math.random() * 3 - 1.5
-        this.color = ["#00FFFF", "#FF00FF", "#FF4D79"][Math.floor(Math.random() * 3)]
-      }
-
-      update() {
-        this.x += this.speedX
-        this.y += this.speedY
-
-        if (this.x > canvas.width) this.x = 0
-        if (this.x < 0) this.x = canvas.width
-        if (this.y > canvas.height) this.y = 0
-        if (this.y < 0) this.y = canvas.height
-      }
-
-      draw() {
-        if (!ctx) return
-        ctx.fillStyle = this.color
-        ctx.beginPath()
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
-        ctx.fill()
-      }
-    }
+    const particleCount = 100
 
     for (let i = 0; i < particleCount; i++) {
-      particles.push(new Particle())
+      particles.push(new Particle(canvas))
     }
 
     function animate() {
-      if (!ctx || !canvas) return
       ctx.clearRect(0, 0, canvas.width, canvas.height)
-
       particles.forEach((particle) => {
-        particle.update()
-        particle.draw()
+        particle.update(canvas)
+        particle.draw(ctx)
       })
-
       requestAnimationFrame(animate)
     }
 
     animate()
 
     const handleResize = () => {
-      if (!canvas) return
       canvas.width = window.innerWidth
       canvas.height = window.innerHeight
     }
 
     window.addEventListener("resize", handleResize)
-    return () => window.removeEventListener("resize", handleResize)
+
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
   }, [])
 
-  return <canvas ref={canvasRef} className="fixed inset-0 pointer-events-none" style={{ zIndex: 0 }} />
+  return <canvas ref={canvasRef} className="fixed top-0 left-0 w-full h-full pointer-events-none" />
 }
+
 
